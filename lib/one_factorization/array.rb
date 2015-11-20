b4e1ae8d8d44419d6f8ec305da5176d48fefcd23
Sam Joseph <tansaku@gmail.com>
2015-11-20 22:10:55 +0000

    refactors code a little

--- lib/one_factorization/array.rb
+++ lib/one_factorization/array.rb
@@ -1,17 +1,13 @@
 class Array
   def one_factorize
     number_pairs = length / 2
-    set = []
     first, *rest = *self
-    rest.each_with_index do |person, index| 
-      pairs = []
-      pairs << [first, person]
-      (1..number_pairs-1).each do |offset|
+    result = rest.each_with_index.inject([]) do |set, (person,index)|
+      set << (1..number_pairs-1).inject([[first, person]]) do |pairs, offset|
         pairs << [rest[(index-offset)%rest.length], rest[(index+offset)%rest.length]]
       end
-      set << pairs
     end
-    fail OneFactorization::InvalidLengthError.new(set) if length.odd?
-    set
+    fail OneFactorization::InvalidLengthError.new(result) if length.odd?
+    result
   end
 end
